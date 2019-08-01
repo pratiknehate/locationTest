704d7d8d4e631d49a4c56e6469a6b2cab1d7c4d1
pratiknehate <53301047+pratiknehate@users.noreply.github.com>
2019-08-01 10:27:32 +0530

    Add files via upload

---
+++ locationweb/src/main/java/com/pratik/location/dto/ReservationRequest.java
@@ -0,0 +1,70 @@
+package com.pratik.location.dto;
+
+public class ReservationRequest {
+	private Long flightId;
+	private String passengerFirstName;
+	private String passengerLastName;
+	private String passengerEmail;
+	private String passengerPhone;
+	private String nameOnCard;
+	private String cardNumber;
+	private String expiryDate;
+	private String CVV;
+	public Long getFlightId() {
+		return flightId;
+	}
+	public void setFlightId(Long flightId) {
+		this.flightId = flightId;
+	}
+	public String getPassengerFirstName() {
+		return passengerFirstName;
+	}
+	public void setPassengerFirstName(String passengerFirstName) {
+		this.passengerFirstName = passengerFirstName;
+	}
+	public String getPassengerLastName() {
+		return passengerLastName;
+	}
+	public void setPassengerLastName(String passengerLastName) {
+		this.passengerLastName = passengerLastName;
+	}
+	public String getPassengerEmail() {
+		return passengerEmail;
+	}
+	public void setPassengerEmail(String passengerEmail) {
+		this.passengerEmail = passengerEmail;
+	}
+	public String getPassengerPhone() {
+		return passengerPhone;
+	}
+	public void setPassengerPhone(String passengerPhone) {
+		this.passengerPhone = passengerPhone;
+	}
+	public String getNameOnCard() {
+		return nameOnCard;
+	}
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public String getCVV() {
+		return CVV;
+	}
+	public void setCVV(String cVV) {
+		CVV = cVV;
+	}
+	
+	
+
+}
